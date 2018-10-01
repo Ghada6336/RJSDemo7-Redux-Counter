d0f3a0fdc86c791899bb9a1e4bc053c0b3685447
Hussein Almutawa <hussein@joincoded.com>
2018-10-01 16:01:17 +0300

    combined reducers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers } from "redux"; //STEP 1
+import counterReducer from "./store/reducers/counter"; //STEP 2
+import logReducer from "./store/reducers/log"; // STEP 2
+
+//STEP 3
+const rootReducer = combineReducers({
+  rootCtr: counterReducer,
+  rootLog: logReducer
+});
+
+const store = createStore(rootReducer); //STEP 4
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
